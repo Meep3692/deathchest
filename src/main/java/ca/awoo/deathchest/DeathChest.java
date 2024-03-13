5b8641e398db6863be8f9994cb3e3eadb9cf6f71
Meep3692 <meep3692@gmail.com>
2024-03-13 16:59:35 -0400

    Delay chest placement by one tick

--- src/main/java/ca/awoo/deathchest/DeathChest.java
+++ src/main/java/ca/awoo/deathchest/DeathChest.java
@@ -5,7 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class DeathChest extends JavaPlugin {
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new DeathListener(), this);
+        getServer().getPluginManager().registerEvents(new DeathListener(this), this);
     }
 
     @Override
--- src/main/java/ca/awoo/deathchest/DeathListener.java
+++ src/main/java/ca/awoo/deathchest/DeathListener.java
@@ -1,31 +1,49 @@
 package ca.awoo.deathchest;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.Chest;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class DeathListener implements Listener{
     
+    private final DeathChest plugin;
+
+    public DeathListener(DeathChest plugin){
+        this.plugin = plugin;
+    }
+
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event){
-        Pair<Location, Location> locations = getFreeSpaceForChest(event.getEntity().getLocation());
-        event.getEntity().getWorld().getBlockAt(locations.getFirst()).setType(Material.CHEST);
-        event.getEntity().getWorld().getBlockAt(locations.getSecond()).setType(Material.CHEST);
-        Inventory inv1 = ((Chest)event.getEntity().getWorld().getBlockAt(locations.getFirst()).getState()).getInventory();
-        Inventory inv2 = ((Chest)event.getEntity().getWorld().getBlockAt(locations.getSecond()).getState()).getInventory();
-        for(int i = 0; i < event.getDrops().size(); i++){
-            if(i < inv1.getSize()){
-                inv1.setItem(i, event.getDrops().get(i));
-            }else{
-                inv2.setItem(i - inv1.getSize(), event.getDrops().get(i));
-            }
-        }
+        //Copy event.getDrop() to a new list and clear the original list
+        List<ItemStack> drops = new ArrayList<>(event.getDrops());
+        Location deathLoc = event.getEntity().getLocation();
+        World world = deathLoc.getWorld();
         event.getDrops().clear();
+        Bukkit.getScheduler().runTaskLater(plugin, () -> {
+            Pair<Location, Location> locations = getFreeSpaceForChest(deathLoc);
+            world.getBlockAt(locations.getFirst()).setType(Material.CHEST);
+            world.getBlockAt(locations.getSecond()).setType(Material.CHEST);
+            Inventory inv1 = ((Chest)world.getBlockAt(locations.getFirst()).getState()).getInventory();
+            Inventory inv2 = ((Chest)world.getBlockAt(locations.getSecond()).getState()).getInventory();
+            for(int i = 0; i < drops.size(); i++){
+                if(i < inv1.getSize()){
+                    inv1.setItem(i, drops.get(i));
+                }else{
+                    inv2.setItem(i - inv1.getSize(), drops.get(i));
+                }
+            }
+        }, 1L);
     }
 
     private Pair<Location, Location> getFreeSpaceForChest(Location deathLoc){
